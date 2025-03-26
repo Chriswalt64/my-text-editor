f782afa9f5906f6c0ad055be5e60255aa1a74dda
Chriswalt64 <walters.chris64@gmail.com>
2025-03-26 14:35:16 -0400

    Removed menu header, added text size options

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
 const mobileNav = document.querySelector(".hamburger");
 const navbar = document.querySelector(".menubar");
 
+function saveFile() {
+  const text = document.getElementById('textArea').value;
+  console.log(text);
+  const blob = new Blob([text], { type: 'text/plain' });
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = 'note.txt';
+  link.click();
+}
+// adding comment to start new branch
+function openFile() {
+  const input = document.createElement('input');
+  input.type = 'file';
+  input.accept = '.txt';
+  input.onchange = (event) => {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+          document.getElementById('textArea').value = reader.result;
+      };
+      reader.readAsText(file);
+  };
+  input.click();
+}
+
+function smallTextSize() {
+  const textArea = document.getElementById("textArea");
+  textArea.style.fontSize = "12px";
+}
+
+function mediumTextSize() {
+  const textArea = document.getElementById("textArea");
+  textArea.style.fontSize = "16px";
+}
+
+function largeTextSize() {
+  const textArea = document.getElementById("textArea");
+  textArea.style.fontSize = "20px";
+}
+
+
 document.querySelector("textarea").addEventListener("keydown", (e) => {
   if (e.key == "Tab") {
     e.preventDefault();
@@ -14,12 +55,7 @@ document.querySelector("textarea").addEventListener("keydown", (e) => {
   }
 });
 
-const toggleNav = () => {
-  navbar.classList.toggle("active");
-  mobileNav.classList.toggle("hamburger-active");
-};
-mobileNav.addEventListener("click", () => toggleNav());
-
+/*
 function saveFile() {
   const text = document.getElementById('textArea').value;
   const blob = new Blob([text], { type: 'text/plain' });
@@ -42,4 +78,4 @@ function openFile() {
       reader.readAsText(file);
   };
   input.click();
-}
+} */
